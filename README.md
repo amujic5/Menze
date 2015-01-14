8cea3d9bb19d835a7f423fb161f96e9869363ecf
Azzaro <mujicazzaro@gmail.com>
2015-01-14 23:29:38 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Menze
+
+Menze is android application for students in Croatia. Application includes:
+
+  - check menu in student's restaurant
+  - check worktime of student's restaurant
+  - check money (saldo) on your X-card
+  - 
+[![Build Status](https://magnum.travis-ci.com/amujic5/Menze.svg?token=yWyVXPj2FGzqd7scZ79F&branch=master)](https://magnum.travis-ci.com/amujic5/Menze)
+
+### Version
+1.0.0
+
+```
+### Todo's
+
+ - Write Tests
+ - Add prices
+ - nutritional value
+
+License
+----
+
+FER
+
+
+
--- app/src/main/java/hr/fer/azzi/menze/fragments/SaldoFragment.java
+++ app/src/main/java/hr/fer/azzi/menze/fragments/SaldoFragment.java
@@ -1,6 +1,6 @@
 package hr.fer.azzi.menze.fragments;
 
-import android.app.Activity;
+
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,7 +8,6 @@ import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,13 +34,11 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import hr.fer.azzi.menze.activitys.MainActivity;
 import hr.fer.azzi.menze.adapters.MeniAdapter;
 import hr.fer.azzi.menze.R;
-import hr.fer.azzi.menze.classes.DateSaldo;
 
 
 /**
@@ -212,7 +209,6 @@ public class SaldoFragment extends Fragment {
                     Toast.makeText(getActivity(), "Trenutni saldo: " + res,Toast.LENGTH_LONG).show();
                 }
             }
-
         }
     }
 
